21123c79e104025daab72184331930a54d2ec3ef
82102 <82102@DESKTOP-4GLAH8M>
2023-02-09 05:50:10 +0900

    vn

---
+++ Test/src/dbutil/ConnectionProvider.java
@@ -0,0 +1,76 @@
+package dbutil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class ConnectionProvider {
+	private static final Properties PROPERTIES = new Properties(); 
+	
+	static {
+		InputStream is = null;
+		try {
+			ClassLoader loader = ConnectionProvider.class.getClassLoader();
+			is = loader.getResourceAsStream("local.properties");
+			
+			PROPERTIES.load(is);
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static Connection makeConnection() throws SQLException {
+		Connection conn = null;
+		conn = DriverManager.getConnection(PROPERTIES.getProperty("url")
+				, PROPERTIES.getProperty("username")
+				, PROPERTIES.getProperty("password"));
+		return conn;
+	}
+	
+	public static void closeConnection(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
